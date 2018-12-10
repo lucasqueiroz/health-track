762bcb8169b19dcbae151d0df5f1e0e8ee552c4b
Lucas Queiroz <lucascqueiroz97@gmail.com>
2018-12-10 21:45:25 -0200

    Fix factory

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -3,8 +3,6 @@ FactoryBot.define do
     name { 'Lucas Queiroz' }
     email { 'lucascqueiroz97@gmail.com' }
     birthday { '26/02/1997' }
-    height { 1.73 }
-    weight { 102 }
     password { 'password' }
   end
 end
