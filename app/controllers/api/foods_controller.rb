a64cfaa55edc2340691973465468d5fad85e7e76
Lucas Queiroz <lucascqueiroz97@gmail.com>
2018-12-11 20:23:25 -0200

    Add show endpoint to foods api

--- app/controllers/api/foods_controller.rb
+++ app/controllers/api/foods_controller.rb
@@ -2,9 +2,21 @@ module Api
   class FoodsController < Api::ApiController
 
     before_action :authenticate_user
+    before_action :set_food, only: [:show]
 
     def index
       json_response(Food.from_user(@authenticated_user))
+    end
+
+    def show
+      json_response(@food)
+    end
+
+    private
+
+    def set_food
+      @food = Food.find_by(id: params[:id], user_id: @authenticated_user.id)
+      json_not_found_response unless @food.present?
     end
   end
 end
