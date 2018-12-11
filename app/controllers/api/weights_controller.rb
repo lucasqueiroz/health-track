25c04c0bbcfa49c27a0ab3cd1466920d8b86cf65
Lucas Queiroz <lucascqueiroz97@gmail.com>
2018-12-11 19:40:22 -0200

    Add create endpoint to weights api

--- app/controllers/api/weights_controller.rb
+++ app/controllers/api/weights_controller.rb
@@ -12,11 +12,25 @@ module Api
       json_response(@weight)
     end
 
+    def create
+      weight = Weight.new(weight_params)
+      weight.user_id = @authenticated_user.id
+      if weight.save
+        json_response(weight)
+      else
+        json_error_response(weight)
+      end
+    end
+
     private
 
     def set_weight
       @weight = Weight.find_by(id: params[:id], user_id: @authenticated_user.id)
       json_not_found_response unless @weight.present?
+    end
+
+    def weight_params
+      params.require(:weight).permit(:measurement, :measured_at)
     end
   end
 end
