cfc3d67d42de3a2c7e2ea8cec2b42523e5ad2dd7
Lucas Queiroz <lucascqueiroz97@gmail.com>
2018-12-11 20:03:29 -0200

    Add show endpoint to workouts api

--- app/controllers/api/workouts_controller.rb
+++ app/controllers/api/workouts_controller.rb
@@ -2,9 +2,21 @@ module Api
   class WorkoutsController < Api::ApiController
 
     before_action :authenticate_user
+    before_action :set_workout, only: [:show]
 
     def index
       json_response(Workout.from_user(@authenticated_user))
+    end
+
+    def show
+      json_response(@workout)
+    end
+
+    private
+
+    def set_workout
+      @workout = Workout.find_by(id: params[:id], user_id: @authenticated_user.id)
+      json_not_found_response unless @workout.present?
     end
   end
 end
--- spec/controllers/api/workouts_controller_spec.rb
+++ spec/controllers/api/workouts_controller_spec.rb
@@ -28,4 +28,69 @@ RSpec.describe Api::WorkoutsController, type: :controller do
       expect(json[0]['user_id']).to eq(workout.user.id)
     end
   end
+
+  describe "GET #show" do
+    before do
+      get :show, params: { id: workout.id }
+    end
+
+    it "returns http success" do
+      expect(response).to have_http_status(:success)
+    end
+
+    it "returns valid JSON body" do
+      expect(json).not_to be_empty
+      expect(json['user_id']).to eq(workout.user_id)
+    end
+
+    context "when user is not the owner of the resource" do
+      before do
+        controller.request.env['HTTP_AUTHORIZATION'] = basic_auth(different_user.email, different_user.password)
+        get :show, params: { id: workout.id }
+      end
+
+      it "returns http not found" do
+        expect(response).to have_http_status(:not_found)
+      end
+
+      it "returns valid JSON body" do
+        expect(json).not_to be_empty
+        expect(json['errors']).not_to be_empty
+        expect(json['errors']).to include('Not found!')
+      end
+    end
+
+    context "when resource does not exist" do
+      before do
+        get :show, params: { id: Workout.last.id + 1 }
+      end
+
+      it "returns http not found" do
+        expect(response).to have_http_status(:not_found)
+      end
+
+      it "returns valid JSON body" do
+        expect(json).not_to be_empty
+        expect(json['errors']).not_to be_empty
+        expect(json['errors']).to include('Not found!')
+      end
+    end
+
+    context "when user is not authorized" do
+      before do
+        controller.request.env['HTTP_AUTHORIZATION'] = basic_auth('email', 'password')
+        get :show, params: { id: workout.id }
+      end
+
+      it "returns http success" do
+        expect(response).to have_http_status(:success)
+      end
+
+      it "returns valid JSON body" do
+        expect(json).not_to be_empty
+        expect(json['errors']).not_to be_empty
+        expect(json['errors']).to include('User not authorized!')
+      end
+    end
+  end
 end
