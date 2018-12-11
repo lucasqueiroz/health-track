4497ccfe8ede0929e1456aab11de11bb774486f3
Lucas Queiroz <lucascqueiroz97@gmail.com>
2018-12-11 19:36:25 -0200

    Add show endpoint to weights api

--- app/controllers/api/weights_controller.rb
+++ app/controllers/api/weights_controller.rb
@@ -2,9 +2,21 @@ module Api
   class WeightsController < Api::ApiController
 
     before_action :authenticate_user
+    before_action :set_weight, only: [:show]
 
     def index
       json_response(Weight.from_user(@authenticated_user))
+    end
+
+    def show
+      json_response(@weight)
+    end
+
+    private
+
+    def set_weight
+      @weight = Weight.find_by(id: params[:id], user_id: @authenticated_user.id)
+      json_not_found_response unless @weight.present?
     end
   end
 end
