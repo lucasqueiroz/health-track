fde01250063b1d1a2489fc4a5ecd4f7e7494b4dd
Lucas Queiroz <lucascqueiroz97@gmail.com>
2018-12-12 00:09:15 -0200

    Increase test coverage on foods controller

--- spec/controllers/foods_controller_spec.rb
+++ spec/controllers/foods_controller_spec.rb
@@ -30,6 +30,17 @@ RSpec.describe FoodsController, type: :controller do
       expect(response).to have_http_status(:found)
       expect(response).to redirect_to(foods_path)
     end
+
+    context "when saving fails" do
+      before do
+        allow_any_instance_of(Food).to receive(:save).and_return(false)
+        post :create, params: { food: { name: 'Poutine', calories: 740, occurred_at: '22/10/2018' } }
+      end
+
+      it "renders the new food page" do
+        expect(response).to render_template(:new)
+      end
+    end
   end
 
   describe "GET #edit" do
@@ -46,6 +57,17 @@ RSpec.describe FoodsController, type: :controller do
       expect(response).to have_http_status(:found)
       expect(response).to redirect_to(foods_path)
       expect(Food.last.calories).to eq(745)
+    end
+
+    context "when updating fails" do
+      before do
+        allow_any_instance_of(Food).to receive(:update).and_return(false)
+        patch :update, params: { id: food.id, food: { calories: 745 } }
+      end
+
+      it "renders the edit food page" do
+        expect(response).to render_template(:edit)
+      end
     end
   end
 
