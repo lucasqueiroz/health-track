f0960515923ec534a988999acf8b1ada34082c7b
Lucas Queiroz <lucascqueiroz97@gmail.com>
2018-12-11 22:01:15 -0200

    Serve assets in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Serve assets in production
+  config.serve_static_assets = true
 end
