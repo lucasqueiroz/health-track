a2e7c3901a6a1fa4024625b7837437a3245490d1
Lucas Queiroz <lucascqueiroz97@gmail.com>
2018-12-11 23:31:06 -0200

    Increase test coverage on sessions controller

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
 
+  let(:user) { create(:user) }
+
   describe "GET #new" do
     it "returns http success" do
       get :new
@@ -10,11 +12,44 @@ RSpec.describe SessionsController, type: :controller do
   end
 
   describe "POST #create" do
+    context "when valid login information is sent" do
+      it "should log in user" do
+        post :create, params: { session: { email: user.email, password: user.password } }
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
     context "when invalid login information is sent" do
       it "should display error message" do
         post :create, params: { session: { email: '', password: '' } }
         expect(response).to render_template(:new)
         expect(flash).not_to be_empty
+      end
+    end
+  end
+
+  describe "DELETE #destroy" do
+    context "when user is logged in" do
+      before do
+        allow_any_instance_of(SessionsHelper).to receive(:logged_in?).and_return(true)
+      end
+
+      it "logs user out" do
+        delete :destroy
+        expect(flash).to be_empty
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context "when user is not logged in" do
+      before do
+        allow_any_instance_of(SessionsHelper).to receive(:logged_in?).and_return(false)
+      end
+
+      it "gives proper error" do
+        delete :destroy
+        expect(flash).not_to be_empty
+        expect(response).to redirect_to(root_path)
       end
     end
   end
