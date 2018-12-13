77e747a5822eb8c98daac0da817fce83e554751a
Lucas Queiroz <lucascqueiroz97@gmail.com>
2018-12-12 23:17:51 -0200

    Reorder user model methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
-  has_secure_password
-  validates :name, presence: true, length: { maximum: 100 }
-  validates :email, presence: true, length: { maximum: 200 }, uniqueness: true, email: true
-  validates :birthday, presence: true, date: { before: Date.new(2005, 1, 1) }
   has_many :heights
   has_many :weights
   has_many :workouts
   has_many :foods
+
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :email, presence: true, length: { maximum: 200 }, uniqueness: true, email: true
+  validates :birthday, presence: true, date: { before: Date.new(2005, 1, 1) }
+
+  has_secure_password
 end
