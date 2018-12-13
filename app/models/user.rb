aeeaf8551f66d8e93de10cdba7f741210c5247cf
Lucas Queiroz <lucascqueiroz97@gmail.com>
2018-12-12 23:25:03 -0200

    Define the dependent option to the has_many association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  has_many :heights
-  has_many :weights
-  has_many :workouts
-  has_many :foods
+  has_many :heights, dependent: :destroy
+  has_many :weights, dependent: :destroy
+  has_many :workouts, dependent: :destroy
+  has_many :foods, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 100 }
   validates :email, presence: true, length: { maximum: 200 }, uniqueness: true, email: true
